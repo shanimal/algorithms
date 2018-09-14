427a053efab276ff212c52345f007792c5352bd1
Shannon Thrasher <283957+shanimal@users.noreply.github.com>
2018-09-13 22:10:36 -0700

    Fibonacci

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,11 +1,20 @@
 /**
- * Fibonacci returns a list of n values where the next number is the sum of the previous two
+ * returns a list of n values where the next number is the sum of the previous two
  * As in fib(13) = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
  * @param {Number} n The number of elements to display
  * @returns {Array} a fib sequence as an array
  */
-const fib = n => [...Array(n).keys()]
+export default const fib = n => [...Array(n)]
   .reduce((a,v) => {
     a.push(a.slice(-1)[0] + a.slice(-2)[0]);
     return a;
   }, [0, 1]);
+
+/**
+ * returns the fibonacci integer at the given depth
+ * @param {Number} n The number of elements to display
+ * @returns {Number} integer at the provided depth
+ */
+export const fibonacci = n => n < 1 ? 0 : (n <= 2 ? 1 : (fibonacci(n - 1) + fibonacci(n - 2)));
+
+
